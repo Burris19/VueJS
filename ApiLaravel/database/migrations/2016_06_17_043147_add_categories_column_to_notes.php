fd5ea7871d0c008b4fc5a4f658ab1fe44ef59236
Burris19 <julianhernandez19@gmail.com>
2016-06-17 06:16:25 +0000

    [add] upadate proyect Laravel 5.2

---
+++ ApiLaravel/database/migrations/2016_06_17_043147_add_categories_column_to_notes.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCategoriesColumnToNotes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->unsignedInteger('category_id')->nullable()->index()->after('note');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropColumn('category_id');
+    }
+}
---
+++ ApiLaravel/database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::create(['name' => 'Laravel']);
+        Category::create(['name' => 'Vue.js']);
+        Category::create(['name' => 'Sass']);
+    }
+}
