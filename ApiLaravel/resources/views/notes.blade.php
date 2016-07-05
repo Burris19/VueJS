8e2017c82791466e679ac187e47acf49532b3621
Burris19 <julianhernandez19@gmail.com>
2016-07-05 15:05:08 -0600

    [add] create modulo app

--- ApiLaravel/resources/views/notes.blade.php
+++ ApiLaravel/resources/views/notes.blade.php
@@ -4,46 +4,9 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <h1>Styde.net / Curso de VueJS</h1>
-      
-      <div class="alert_container">        
-        <p  v-show="alert.display"
-            class="alert alert-danger" 
-            id="error_message" 
-            transition="fade" >@{{ alert.message }}</p>
-
-      </div>
-      <table class="table table-striped">
-        <tr>
-          <th>Categoría</th>
-          <th>Nota</th>
-          <th width="50px">&nbsp;</th>
-        </tr>
-        <tr 
-            v-for="note in notes" 
-            is="note-row" 
-            :note.sync="note" 
-            :categories="categories"
-            @update-note="updateNote"
-            @delete-note="deleteNote"></tr>
-        <tr>
-            <td><select-category :categories="categories" :id.sync="new_note.category_id"></select-category></td>
-            <td>
-                <input type="text" v-model="new_note.note" class="form-control">
-                <ul v-if="errors.length" class="text-danger">  
-                    <li v-for="error in errors">@{{ error }}</li>
-                </ul>
-            </td>
-            <td>
-              <a href="#" @click="createNote">
-                <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-              </a>
-            </td>
-        </tr>
-      </table>
+      <app></app>
     </div>
   </div>
-
-  <pre>@{{ $data | json }}</pre>
 @endsection
 
 
@@ -64,11 +27,9 @@
       height:  60px
     }
 
-  </style>
-  
+  </style>  
 
 <script type="text/javascript" src="js/main.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-
 
 @endsection()
